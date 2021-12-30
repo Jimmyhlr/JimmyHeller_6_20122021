5c6057ef0905a5412f9ca32bc61cf59dc63ffb02
Jimmyhlr <heller.jimmy.ci@gmail.com>
2021-12-30 16:52:34 +0100

    Setting base Express application

---
+++ back/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+// importation de Express
+
+const app = express();
+// crée une application Express
+
+module.exports = app;
+// exporte l'application pour qu'elle soit accessible depuis les autres fichiers du projet
